0ea3e75b7eaa1d881db042476f961ca0c3fbaf21
Pejo Gusak <pgusak@hr.asseco-see.local>
2020-01-23 10:06:15 +0100

    LRI | added vivado and xilinx sdk project files

---
+++ vivado-project/project_1.sdk/kamera/Debug/src/helloworld.d
@@ -0,0 +1,59 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xiicps.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xiicps_hw.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xplatform_info.h ../src/camera.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xgpio_l.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xiicps.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xiicps_hw.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../src/camera.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xgpio_l.h:
---
+++ vivado-project/project_1.sdk/kamera/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../slikaj_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../slikaj_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
